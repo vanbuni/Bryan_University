c51915176afef47e030e93c767de8aea59e3eeae
Lavan Bradley <you@example.com>
2022-01-09 15:37:03 -0800

    dajango project

--- afs200/week5/pauseAndPractice.py
+++ afs200/week5/pauseAndPractice.py
@@ -51,7 +51,7 @@
 # #lambda reduce
 
 
-from functools import reduce
-sequences = [1,2,3,4,5]
-product = reduce (lambda x, y: x*y, sequences)
-print(product)
+# from functools import reduce
+# sequences = [1,2,3,4,5]
+# product = reduce (lambda x, y: x*y, sequences)
+# print(product)
